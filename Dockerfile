d5c979734c7c3f5eb9575156882a7ccb6abefcd6
Yummiii <Leeo05050@protonmail.com>
2024-03-31 17:50:03 -0300

    fix docker compose

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM nvidia/cuda:12.3.2-runtime-ubuntu22.04
+LABEL org.opencontainers.image.source https://github.com/Yummiii/sd-webui-forge-docker
 WORKDIR /app
 RUN apt update && apt upgrade -y
 RUN apt install -y wget git python3 python3-venv libgl1 libglib2.0-0 apt-transport-https libgoogle-perftools-dev bc
--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ echo "Starting Stable Diffusion WebUI"
 if [ ! -d "/app/sd-webui" ] || [ ! "$(ls -A "/app/sd-webui")" ]; then
   echo "Files not found, cloning..."
 
-  git clone https://github.com/lllyasviel/stable-diffusion-webui-forge /app/sd-webui
+  git clone https://github.com/lllyasviel/stable-diffusion-webui-forge.git /app/sd-webui
   chmod +x /app/sd-webui/webui.sh
   
   exec /app/sd-webui/webui.sh $ARGS
