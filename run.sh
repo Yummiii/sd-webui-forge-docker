c64f5e6b8e92ac3091839a251a04ed255b5ef7e9
Yummiii <Leeo05050@protonmail.com>
2024-05-25 17:41:50 -0300

    fix insightface update

--- run.sh
+++ run.sh
@@ -6,6 +6,13 @@ if [ ! -d "/app/sd-webui" ] || [ ! "$(ls -A "/app/sd-webui")" ]; then
   git clone https://github.com/lllyasviel/stable-diffusion-webui-forge.git /app/sd-webui
   chmod +x /app/sd-webui/webui.sh
   cd /app/sd-webui
+
+  #i don't really know if this is the best way to do this
+  python3 -m venv venv
+  source ./venv/bin/activate
+  pip install insightface
+  deactivate
+
   exec /app/sd-webui/webui.sh $ARGS
 else
   echo "Files found, starting..."
